862201e8d2957aa57e1a809413383345e947023f
MaxPrest <maximprestwich@outlook.com>
2022-10-25 12:47:48 +0100

    started the project_tracker app and created the project and ticket models

---
+++ project_tracker/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    date_created = models.DateField(auto_now_add=True)
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+class Tickets(models.Model):
+    STATUS_OPEN = 'O'
+    STATUS_STARTED = 'S'
+    STATUS_CLOSED = 'C'
+    
+    STATUS_CHOICES = [
+        (STATUS_OPEN, 'open'),
+        (STATUS_STARTED, 'started'),
+        (STATUS_CLOSED, 'closed')
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    submitter = models.CharField(max_length=255)
+    developer = models.CharField(max_length=255)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_OPEN)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
