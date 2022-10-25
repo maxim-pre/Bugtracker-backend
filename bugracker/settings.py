c94ae10991fb5c8a4b9a814a4420a525c043d0c7
MaxPrest <maximprestwich@outlook.com>
2022-10-25 13:17:15 +0100

    installed mysql client and made inital migrations to the DB

--- bugracker/settings.py
+++ bugracker/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bugracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_tracker',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PASSWORD': 'mp140401',
+
     }
 }
 
