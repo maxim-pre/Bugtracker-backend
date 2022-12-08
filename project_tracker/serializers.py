2e4e4b123efa8af5b9c605fae3a9a67d23846ad7
MaxPrest <maximprestwich@outlook.com>
2022-12-08 14:45:26 +0000

    finished the create ticket serializer

--- project_tracker/serializers.py
+++ project_tracker/serializers.py
@@ -1,12 +1,31 @@
 from django.db import models
 from rest_framework import serializers
-from .models import Project, ProjectDeveloper, Ticket, Developer
-from django.conf import settings
+from .models import Project, ProjectDeveloper, Ticket, Developer, TicketDeveloper
+from django.contrib.auth import get_user_model
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        fields = ['id','username','email', 'first_name','last_name']
+        model = get_user_model()
+
+class DeveloperSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        fields = ['id', 'user', 'phone', 'birthday',]
+        model = Developer
+
+class ProjectDeveloperSerializer(serializers.ModelSerializer):
+    developer = DeveloperSerializer()
+    class Meta:
+        fields = ['developer']
+        model = ProjectDeveloper
 
 class ProjectSerializer(serializers.ModelSerializer):
+    creator = DeveloperSerializer()
+    developers = ProjectDeveloperSerializer(many=True)
     class Meta:
-        fields = ['id', 'name', 'description', 'creator', 'date_created']
+        fields = ['id', 'name', 'description', 'creator', 'date_created', 'developers']
         model = Project
 
 class CreateProjectSerializer(serializers.ModelSerializer):
@@ -34,68 +53,103 @@ class UpdateProjectSerializer(serializers.ModelSerializer):
         model = Project
         
 
-class TicketSerializer(serializers.ModelSerializer):
+
+class TicketDeveloperSerializer(serializers.ModelSerializer):
+    developer = DeveloperSerializer()
     class Meta:
-        fields = ['id', 'title','description','submitter','developer','status','created_at','last_updated','project']
+        fields = ['developer']
+        model = TicketDeveloper
+
+class TicketSerializer(serializers.ModelSerializer):
+    developers = TicketDeveloperSerializer(many=True)
+    submitter = DeveloperSerializer()
+    class Meta:
+        fields = ['id', 'title','description','submitter','developers','status', 'type', 'priority','created_at','last_updated','project']
         model = Ticket
 
 class CreateTicketSerializer(serializers.ModelSerializer):
-    developer_id = serializers.IntegerField()
+
+    description = serializers.CharField(max_length=255)
+    developers = serializers.ListField(
+        child=serializers.CharField(max_length=255)
+    )
     class Meta:
-        fields = ['title','description', 'developer_id']
+        fields = ['title','description', 'developers', 'type','priority']
         model = Ticket
-    
-    def validate_developer_id(self, value):
-        if not ProjectDeveloper.objects.filter(project_id=self.context['project_id'], developer_id=value):
-            raise serializers.ValidationError('you cannot assign that ticket to a developer that is not part of that project')
-        return value
 
     def save(self, **kwargs):
+
+        User = get_user_model()
         submitter = Developer.objects.get(user_id=self.context['user_id'])
         project = Project.objects.get(id=self.context['project_id'])
-        developer = self.validated_data['developer_id']
 
         new_ticket = Ticket.objects.create(
+            project = project,
+            submitter = submitter,
             title = self.validated_data['title'],
             description = self.validated_data['description'],
-            submitter = submitter,
-            developer_id = developer,
-            project = project
+            type = self.validated_data['type'],
+            priority = self.validated_data['priority']
         )
+
+        usernames = self.validated_data['developers'] # this will be a list of usernames 
+        user_ids = User.objects.prefetch_related("developers").only('id').filter(username__in=usernames)
+        developer_ids = Developer.objects.only('id').filter(user_id__in=user_ids)
+
+        for developer in developer_ids:
+            TicketDeveloper.objects.create(
+                ticket_id = new_ticket.id,
+                developer_id = developer.id
+            )
         return 
 
 
-class DeveloperSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField(read_only=True)
-    class Meta:
-        fields = ['id', 'user_id', 'phone', 'birthday']
-        model = Developer
-
 class CreateProjectDeveloperSerializer(serializers.Serializer):
-    developer_id = serializers.IntegerField()
+    # developer_id = serializers.IntegerField()
+    username = serializers.CharField()
     
-    def validate_developer_id(self, developer_id):
-        #make sure the developer exists
-        if not Developer.objects.filter(pk=developer_id).exists():
-            raise serializers.ValidationError('error: developer with that id does not exist')
-        #make sure the developer is not already part of the project
-        invalid_ids = [entry.id for entry in ProjectDeveloper.objects \
+    
+    def validate_username(self, username):
+        User = get_user_model()
+        #make sure the user exists
+        if not User.objects.filter(username=username).exists():
+            raise serializers.ValidationError('developer with that username does not exist')
+        #make sure the user is not already part of the project
+        user = User.objects.get(username=username)
+        developer = Developer.objects.get(user_id=user.id)
+        
+
+        invalid_developer_ids = [entry.developer_id for entry in ProjectDeveloper.objects \
                         .only('developer_id') \
                         .filter(project_id=self.context['project_id'])]
-        if developer_id in invalid_ids:
-            raise serializers.ValidationError(f'error: developer with id {developer_id} is already part of this project')
+
+        if developer.id in invalid_developer_ids:
+            raise serializers.ValidationError(f'{username} is already part of this project')
         
-        return developer_id
+        return username
 
 
     
     def save(self, **kwargs):
-        developer_id = self.validated_data['developer_id']
+        # developer_id = self.validated_data['developer_id']
+        username = self.validated_data['username']
         project_id = self.context['project_id']
+        # find the developer_id based on the provided username
+        try:
+            User = get_user_model()
+            user_id = User.objects.get(username=username).id
+            developer = Developer.objects.get(user_id=user_id)
+        except:
+            raise serializers.ValidationError('A developer with that username does not exist')
 
         return ProjectDeveloper.objects.create(
             project_id=project_id,
-            developer_id=developer_id
+            developer_id = developer.id
         )
+
+        # return ProjectDeveloper.objects.create(
+        #     project_id=project_id,
+        #     developer_id=developer_id
+        # )
 
 
