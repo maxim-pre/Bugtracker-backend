77d781accf50d10fb4683187281340911938747b
MaxPrest <maximprestwich@outlook.com>
2023-02-05 14:48:40 +0000

    collected static files

--- bugracker/settings/common.py
+++ bugracker/settings/common.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,6 +117,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
