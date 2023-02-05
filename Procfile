04117cd5a304de89e0d212460a0a59dba88383d6
MaxPrest <maximprestwich@outlook.com>
2023-02-05 14:32:46 +0000

    added the procfile, heroku configurations, production settings

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: waitress-serve --port=8000 bugracker.wsgi:application
--- bugracker/settings/prod.py
+++ bugracker/settings/prod.py
@@ -1,8 +1,13 @@
 import os
 from .common import *
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bugtracker-prod.herokuapp.com"]
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
