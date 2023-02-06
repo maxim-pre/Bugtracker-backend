02798ee2b0e94a9e521d82479ddd7693feefeaf3
MaxPrest <maximprestwich@outlook.com>
2023-02-06 14:38:13 +0000

    added the front-end to the list of allowed hosts

--- bugracker/settings/prod.py
+++ bugracker/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ["bugtracker-prod.herokuapp.com"]
+ALLOWED_HOSTS = ["bugtracker-prod.herokuapp.com", "bugtrackeradmin.herokuapp.com"]
 
 DATABASES = {
     'default': dj_database_url.config()
